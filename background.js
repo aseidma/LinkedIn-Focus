23fb6b73feea77b4d25faa9b3f296dad21eddad5
ArsalaBangash <arsalabangash@gmail.com>
2019-10-05 21:06:50 -0400

    Added base functionality

--- background.js
+++ background.js
@@ -1,10 +1,18 @@
-// When the browser-action button is clicked...
+/**
+ * We log to the console to ensure our extension was correctly installed. 
+ */
+chrome.runtime.onInstalled.addListener(function () {
+    console.log("You must Focus!");
+});
+
+/**
+ * When the Linkedin InFocus extension button is clicked, we
+ * send a 'block_news' message to the content scripts on our 
+ * tab to hide the elements we want to get rid of. 
+ */
 chrome.browserAction.onClicked.addListener(function (tab) {
-    
+    console.log("Extension button clicked")
     chrome.tabs.sendMessage(tab.id, { text: 'block_news' });
     console.log("Block news message sent")
 });
 
-chrome.runtime.onInstalled.addListener(function () {
-    console.log("You must Focus!");
-});
--- content.js
+++ content.js
@@ -1,9 +1,13 @@
-// Listen for messages
+/**
+ * Our content script can interact with the DOM, so we register a listener 
+ * that hides the news feed and side news panel when the 'block_news' message
+ * is sent by the background script.
+ */
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    // If the received message has the expected format...
     if (msg.text === 'block_news') {
-        console.log("Blocking news")
-        // Hide the HTML element with class name feed-shared-news-module
+        // We hide the HTML elements with class names that match the news 
+        // feed and emerging news panel
         document.getElementsByClassName('feed-shared-news-module')[0].style.display = 'none'
-    }
+        document.getElementsByClassName('core-rail')[0].style.display = 'none'
+}
 });
