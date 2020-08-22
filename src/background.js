76a75ed60cddf601a2922b298d3d93336ae1b89b
ArsalaBangash <arsalabangash@gmail.com>
2020-08-21 23:45:05 -0400

    Added a listener for tab updates so that the feeds are blocked when navigating back to home

--- src/background.js
+++ src/background.js
@@ -1,17 +1,34 @@
 var port;
 var focus = true;
 
-
 chrome.runtime.onConnect.addListener(function (connectionPort) {
     console.assert(connectionPort.name == "linkedin-focus");
     port = connectionPort
-    port.postMessage({type: "focus"})
+    port.postMessage({ type: "focus" })
 });
 
 chrome.browserAction.onClicked.addListener(function () {
-    if (! focus) {
-        port.postMessage({type: "focus"})
+    if (!focus) {
+        port.postMessage({ type: "focus" })
     } else {
-        port.postMessage({type: "unfocus"})
-    } focus = ! focus;
+        port.postMessage({ type: "unfocus" })
+    } focus = !focus;
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (!port) {
+        return
+    }
+    if (changeInfo.url) {
+        console.log("Url exists")
+        if (changeInfo.url.includes("linkedin.com/feed")) {
+            console.log("LinkedIn feed")
+            if (focus) {
+                console.log("Focused")
+                port.postMessage({ type: "focus" })
+            }
+        } else {
+            port.postMessage({ type: "reset" })
+        }
+    }
 });
